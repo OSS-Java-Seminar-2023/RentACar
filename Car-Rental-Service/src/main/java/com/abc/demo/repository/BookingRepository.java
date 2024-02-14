10cc4ef967900cee0ce3aed4774bd195d3a6ee5e
Branimir Ivanda <brane.ivanda@gmail.com>
2024-02-14 16:06:36 +0100

    Commit

---
+++ Car-Rental-Service/src/main/java/com/abc/demo/repository/BookingRepository.java
@@ -0,0 +1,18 @@
+package com.abc.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.abc.demo.dto.BookingDTO;
+@Repository
+public interface BookingRepository extends JpaRepository<BookingDTO, Long> {
+
+	BookingDTO findByBookingDate(String string);
+	@Query("select b from BookingDTO b where b.userId=?1")
+	List<BookingDTO> getAllBookingById(long id);
+	BookingDTO findByBookingNo(long andIncrement);
+	
+}
